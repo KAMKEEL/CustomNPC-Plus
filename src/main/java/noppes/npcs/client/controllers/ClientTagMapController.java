418d345293fe218761c2951926b658e008b3c1ba
KAMKEEL <KamronVaswani@gmail.com>
2023-03-22 13:19:08 -0400

    Create ClientTagMapController.java

---
+++ src/main/java/noppes/npcs/client/controllers/ClientTagMapController.java
@@ -0,0 +1,19 @@
+package noppes.npcs.client.controllers;
+
+import noppes.npcs.CustomNpcs;
+import noppes.npcs.controllers.ServerCloneController;
+import noppes.npcs.controllers.ServerTagMapController;
+
+import java.io.File;
+
+public class ClientTagMapController extends ServerTagMapController {
+	public static ClientTagMapController Instance;
+
+	@Override
+	public File getDir(){
+		File dir = new File(CustomNpcs.Dir,"clones");
+		if(!dir.exists())
+			dir.mkdir();
+		return dir;
+	}
+}
