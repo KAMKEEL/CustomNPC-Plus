ceac11d2518254e000211ed41d22d3f77ab97cb9
Kam <KamronGaming@gmail.com>
2023-05-12 14:26:45 -0400

    Fix Tracked Quest null crash

--- src/main/java/noppes/npcs/quests/QuestInterface.java
+++ src/main/java/noppes/npcs/quests/QuestInterface.java
@@ -10,6 +10,7 @@ import noppes.npcs.constants.EnumPacketClient;
 import noppes.npcs.controllers.PlayerDataController;
 import noppes.npcs.controllers.QuestController;
 import noppes.npcs.controllers.data.PlayerData;
+import noppes.npcs.controllers.data.PlayerQuestData;
 
 import java.util.Vector;
 
@@ -19,8 +20,11 @@ public abstract class QuestInterface implements IQuestInterface {
 	public abstract void readEntityFromNBT(NBTTagCompound compound);
 	public abstract boolean isCompleted(PlayerData player);
 	public void handleComplete(EntityPlayer player) {
-		if (this.questId == PlayerDataController.instance.getPlayerData(player).questData.getTrackedQuest().getId()) {
-			PlayerDataController.instance.getPlayerData(player).questData.untrackQuest();
+		PlayerQuestData questData = PlayerDataController.instance.getPlayerData(player).questData;
+		if(questData != null && questData.getTrackedQuest() != null){
+			if (this.questId == PlayerDataController.instance.getPlayerData(player).questData.getTrackedQuest().getId()) {
+				PlayerDataController.instance.getPlayerData(player).questData.untrackQuest();
+			}
 		}
 	}
 	public abstract Vector<String> getQuestLogStatus(EntityPlayer player);
