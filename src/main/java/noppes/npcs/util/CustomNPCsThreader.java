5d4410e790641bb0f8d6b140bdc188dd69054db5
Kam <KamronGaming@gmail.com>
2023-06-04 22:05:11 -0400

    Unused Thread

--- src/main/java/noppes/npcs/util/CustomNPCsThreader.java
+++ src/main/java/noppes/npcs/util/CustomNPCsThreader.java
@@ -6,6 +6,5 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class CustomNPCsThreader {
-	private final static ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 	public static final Executor playerDataThread = Executors.newSingleThreadExecutor();
 }
