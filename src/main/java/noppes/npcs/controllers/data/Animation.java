20eb4cd1abd86fb539b379c08e8b0b09d5ceb0c5
Kam <KamronGaming@gmail.com>
2023-11-15 15:20:27 -0500

    Fix NullPointer in New Animation Frames!

--- src/main/java/noppes/npcs/controllers/data/Animation.java
+++ src/main/java/noppes/npcs/controllers/data/Animation.java
@@ -255,9 +255,13 @@ public class Animation implements IAnimation {
 	public void jumpToCurrentFrame() {
 		this.currentFrameTime = 0;
 		Frame frame = (Frame) this.currentFrame();
-		for (EnumAnimationPart part : EnumAnimationPart.values()) {
-			if (frame.frameParts.containsKey(part)) {
-				frame.frameParts.get(part).jumpToCurrentFrame();
+		if(frame != null){
+			for (EnumAnimationPart part : EnumAnimationPart.values()) {
+				if(part != null){
+					if (frame.frameParts.containsKey(part)) {
+						frame.frameParts.get(part).jumpToCurrentFrame();
+					}
+				}
 			}
 		}
 	}
