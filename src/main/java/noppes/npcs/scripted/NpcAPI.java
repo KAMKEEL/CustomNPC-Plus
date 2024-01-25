0f41c11e892df21654fc546c67743e469d264f49
Kam <KamronGaming@gmail.com>
2024-01-25 15:55:03 -0500

    Remove decompiler message

--- src/main/java/noppes/npcs/scripted/NpcAPI.java
+++ src/main/java/noppes/npcs/scripted/NpcAPI.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package noppes.npcs.scripted;
 
 import cpw.mods.fml.common.eventhandler.EventBus;
