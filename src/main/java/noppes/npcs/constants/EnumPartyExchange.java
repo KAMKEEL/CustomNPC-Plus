fc4ec5595a35a4cc5c436ebf38dcf87484c07256
Kam <KamronGaming@gmail.com>
2024-02-07 22:41:38 -0500

    Refactor all Scripting Party Helpers

---
+++ src/main/java/noppes/npcs/constants/EnumPartyExchange.java
@@ -0,0 +1,23 @@
+package noppes.npcs.constants;
+
+import java.util.ArrayList;
+
+public enum EnumPartyExchange {
+	Leader("party.leader"),
+	All("party.all"),
+	Enrolled("party.enrolled"),
+	Completer("party.completer");
+
+	String name;
+
+	EnumPartyExchange(String name){
+		this.name = name;
+	}
+	public static String[] names(){
+		ArrayList<String> list = new ArrayList<String>();
+		for(EnumPartyExchange e : values())
+			list.add(e.name);
+		
+		return list.toArray(new String[list.size()]);
+	}
+}
---
+++ src/main/java/noppes/npcs/constants/EnumPartyRequirements.java
@@ -0,0 +1,23 @@
+package noppes.npcs.constants;
+
+import java.util.ArrayList;
+
+public enum EnumPartyRequirements {
+	Leader("party.leader"),
+	All("party.all"),
+	Enrolled("party.enrolled"),
+	Valid("party.valid");
+
+	String name;
+
+	EnumPartyRequirements(String name){
+		this.name = name;
+	}
+	public static String[] names(){
+		ArrayList<String> list = new ArrayList<String>();
+		for(EnumPartyRequirements e : values())
+			list.add(e.name);
+		
+		return list.toArray(new String[list.size()]);
+	}
+}
