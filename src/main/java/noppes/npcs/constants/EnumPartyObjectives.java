992b0e82760b8f4f4c02f5ce98d3326d33d5fb1c
Kam <KamronGaming@gmail.com>
2024-03-03 17:38:25 -0500

    Create EnumPartyObjectives.java

---
+++ src/main/java/noppes/npcs/constants/EnumPartyObjectives.java
@@ -0,0 +1,22 @@
+package noppes.npcs.constants;
+
+import java.util.ArrayList;
+
+public enum EnumPartyObjectives {
+    Shared("party.shared"),
+    All("party.all"),
+	Leader("party.leader");
+
+	String name;
+
+	EnumPartyObjectives(String name){
+		this.name = name;
+	}
+	public static String[] names(){
+		ArrayList<String> list = new ArrayList<String>();
+		for(EnumPartyObjectives e : values())
+			list.add(e.name);
+
+		return list.toArray(new String[list.size()]);
+	}
+}
