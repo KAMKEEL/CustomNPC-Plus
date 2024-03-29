b05b3bdcbc57ba5b1209f9a62f0cd2bdb5288959
Kam <KamronGaming@gmail.com>
2024-03-29 01:43:52 -0400

    1.9.2

--- src/main/java/noppes/npcs/CustomNpcs.java
+++ src/main/java/noppes/npcs/CustomNpcs.java
@@ -36,7 +36,7 @@ import noppes.npcs.scripted.NpcAPI;
 import java.io.File;
 import java.util.Set;
 
-@Mod(modid = "customnpcs", name = "CustomNPC+", version = "1.9.1")
+@Mod(modid = "customnpcs", name = "CustomNPC+", version = "1.9.2")
 public class CustomNpcs {
 
     @SidedProxy(clientSide = "noppes.npcs.client.ClientProxy", serverSide = "noppes.npcs.CommonProxy")
