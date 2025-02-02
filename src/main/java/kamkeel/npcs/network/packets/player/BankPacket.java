87eeebc1afacd1a637aaec0744ec0807e5eebb70
somehussar <148253652+somehussar@users.noreply.github.com>
2025-02-02 23:08:23 +0100

    Remove unused imports

--- src/main/java/kamkeel/npcs/network/packets/player/BankPacket.java
+++ src/main/java/kamkeel/npcs/network/packets/player/BankPacket.java
@@ -10,11 +10,9 @@ import kamkeel.npcs.network.enums.EnumPlayerPacket;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import noppes.npcs.NoppesUtilPlayer;
-import noppes.npcs.NoppesUtilServer;
 import noppes.npcs.constants.EnumRoleType;
 import noppes.npcs.controllers.PlayerDataController;
 import noppes.npcs.controllers.data.BankData;
-import noppes.npcs.entity.EntityNPCInterface;
 
 import java.io.IOException;
 
