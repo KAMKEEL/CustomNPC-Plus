8d55502f6e61af4ef3308ffd8def5bf9597eb5b2
Kam <KamronGaming@gmail.com>
2025-03-14 16:03:06 -0400

    If DBC Addon installed handle Animations on DBC Addon End

--- src/main/java/noppes/npcs/AnimationMixinFunctions.java
+++ src/main/java/noppes/npcs/AnimationMixinFunctions.java
@@ -52,7 +52,7 @@ public class AnimationMixinFunctions {
             return false;
         }
 
-        if (ClientEventHandler.renderingPlayer != null) {
+        if (!DBCAddon.IsAvailable() && ClientEventHandler.renderingPlayer != null) {
             ClientEventHandler.playerModel = (modelRenderer).baseModel;
             if (ClientCacheHandler.playerAnimations.containsKey(ClientEventHandler.renderingPlayer.getUniqueID())) {
                 AnimationData animData = ClientCacheHandler.playerAnimations.get(ClientEventHandler.renderingPlayer.getUniqueID());
@@ -221,6 +221,10 @@ public class AnimationMixinFunctions {
     }
 
     public static boolean mixin_renderFirstPersonAnimation(float partialRenderTick, EntityPlayer player, ModelBiped model, RenderBlocks renderBlocksIr, ResourceLocation resItemGlint) {
+        if (!DBCAddon.IsAvailable()) {
+            return false;
+        }
+
         AnimationData animationData = ClientCacheHandler.playerAnimations.get(player.getUniqueID());
         if (animationData != null && animationData.isActive()) {
             Frame frame = (Frame) animationData.animation.currentFrame();
@@ -898,9 +902,9 @@ public class AnimationMixinFunctions {
                 tailCol = (int) JRMCoreH.getMethod(
                     "stat", Entity.class, int.class, int.class, int.class, int.class, int.class, int.class, float.class).invoke(null,
                     par1EntityPlayer, 2,
-                    (byte) JRMCoreH.getField("Pwrtyp").get(null), 2,
+                    JRMCoreH.getField("Pwrtyp").get(null), 2,
                     ((int[]) JRMCoreH.getField("PlyrAttrbts").get(null))[2], race,
-                    (byte) JRMCoreH.getField("Class").get(null), 0.0F);
+                    JRMCoreH.getField("Class").get(null), 0.0F);
                 jx = Integer.parseInt((String) JRMCoreH.getMethod("data", String.class, int.class, String.class).invoke(null, par1EntityPlayer.getCommandSenderName(), 8, "200"));
                 float one = (float) tailCol / 100.0F;
                 int perc = (int) ((float) jx / one);
