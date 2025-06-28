6de09c1ee430435efe9d372e05f20a5e10a32c44
bigguy345 <monkeybar3200@gmail.com>
2025-06-28 09:38:38 +0300

    Added player's ActionManager getter to API

--- src/main/java/noppes/npcs/scripted/entity/ScriptPlayer.java
+++ src/main/java/noppes/npcs/scripted/entity/ScriptPlayer.java
@@ -27,6 +27,7 @@ import noppes.npcs.api.*;
 import noppes.npcs.api.entity.IEntity;
 import noppes.npcs.api.entity.IPlayer;
 import noppes.npcs.api.gui.ICustomGui;
+import noppes.npcs.api.handler.IActionManager;
 import noppes.npcs.api.handler.IOverlayHandler;
 import noppes.npcs.api.handler.data.*;
 import noppes.npcs.api.item.IItemStack;
@@ -845,5 +846,10 @@ public class ScriptPlayer<T extends EntityPlayerMP> extends ScriptLivingBase<T> 
 
     public IPlayerAttributes getAttributes() {
         return AttributeController.getTracker(this.player);
+    }
+
+    @Override
+    public IActionManager getActionManager() {
+        return data.actionManager;
     }
 }
