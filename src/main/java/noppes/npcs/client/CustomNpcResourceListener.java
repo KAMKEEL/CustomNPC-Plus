61dc0567356fc4cdebb549fac5117355ac3e1e19
Kam <KamronGaming@gmail.com>
2025-07-23 14:17:21 -0400

    Fix some Render handling

--- src/main/java/noppes/npcs/client/CustomNpcResourceListener.java
+++ src/main/java/noppes/npcs/client/CustomNpcResourceListener.java
@@ -14,6 +14,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 
 public class CustomNpcResourceListener implements
     IResourceManagerReloadListener {
--- src/main/java/noppes/npcs/client/renderer/RenderNPCPony.java
+++ src/main/java/noppes/npcs/client/renderer/RenderNPCPony.java
@@ -24,6 +24,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class RenderNPCPony extends RenderNPCInterface {
 
