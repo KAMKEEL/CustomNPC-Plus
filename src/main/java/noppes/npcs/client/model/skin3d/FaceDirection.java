78c8aa8e37b3bb9f468edb919ad31495f422672f
Kam <KamronVaswani@gmail.com>
2025-07-24 16:51:35 -0400

    Fix face direction and remove unused matrix calls

--- src/main/java/noppes/npcs/client/model/skin3d/FaceDirection.java
+++ src/main/java/noppes/npcs/client/model/skin3d/FaceDirection.java
@@ -1,25 +1,28 @@
 package noppes.npcs.client.model.skin3d;
 
-import net.minecraft.util.Vec3i;
 import net.minecraft.util.Vec3;
 
 public enum FaceDirection {
-    DOWN(new Vec3i(0, -1, 0)),
-    UP(new Vec3i(0, 1, 0)),
-    NORTH(new Vec3i(0, 0, -1)),
-    SOUTH(new Vec3i(0, 0, 1)),
-    WEST(new Vec3i(-1, 0, 0)),
-    EAST(new Vec3i(1, 0, 0));
+    DOWN(0, -1, 0),
+    UP(0, 1, 0),
+    NORTH(0, 0, -1),
+    SOUTH(0, 0, 1),
+    WEST(-1, 0, 0),
+    EAST(1, 0, 0);
 
-    private final Vec3i normal;
+    private final int stepX;
+    private final int stepY;
+    private final int stepZ;
 
-    FaceDirection(Vec3i normal) {
-        this.normal = normal;
+    FaceDirection(int x, int y, int z) {
+        this.stepX = x;
+        this.stepY = y;
+        this.stepZ = z;
     }
 
-    public int getStepX() { return normal.getX(); }
-    public int getStepY() { return normal.getY(); }
-    public int getStepZ() { return normal.getZ(); }
+    public int getStepX() { return stepX; }
+    public int getStepY() { return stepY; }
+    public int getStepZ() { return stepZ; }
 
-    public Vec3 step() { return Vec3.createVectorHelper(getStepX(), getStepY(), getStepZ()); }
+    public Vec3 step() { return Vec3.createVectorHelper(stepX, stepY, stepZ); }
 }
--- src/main/java/noppes/npcs/client/model/skin3d/PixelModelPart.java
+++ src/main/java/noppes/npcs/client/model/skin3d/PixelModelPart.java
@@ -3,13 +3,9 @@ package noppes.npcs.client.model.skin3d;
 import java.util.List;
 
 import net.minecraft.client.renderer.Tessellator;
-import org.lwjgl.opengl.GL11;
 
 public class PixelModelPart {
     private final List<PixelCube> cubes;
-    public float x;
-    public float y;
-    public float z;
     public boolean visible = true;
 
     public PixelModelPart(List<PixelCube> cubes) {
@@ -18,12 +14,9 @@ public class PixelModelPart {
 
     public void render() {
         if(!visible) return;
-        GL11.glPushMatrix();
-        GL11.glTranslatef(x/16f, y/16f, z/16f);
         Tessellator tess = Tessellator.instance;
         for(PixelCube cube : cubes) {
             cube.render(tess);
         }
-        GL11.glPopMatrix();
     }
 }
