19d6ec7e5262adbc67bcb9610f6fc412d5db6df1
Kam <KamronGaming@gmail.com>
2026-03-11 16:04:56 -0400

    1.11 Release

--- src/main/java/noppes/npcs/CustomNpcs.java
+++ src/main/java/noppes/npcs/CustomNpcs.java
@@ -125,7 +125,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Consumer;
 
-@Mod(modid = "customnpcs", name = "CustomNPC+", version = "1.11-final-pre-release")
+@Mod(modid = "customnpcs", name = "CustomNPC+", version = "1.11")
 public class CustomNpcs {
 
     @SidedProxy(clientSide = "noppes.npcs.client.ClientProxy", serverSide = "noppes.npcs.CommonProxy")
